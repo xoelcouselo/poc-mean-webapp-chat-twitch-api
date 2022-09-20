b8da8af8d785044a2056c31acd23a57ffee0c32c
Xoel Couselo <45366808+xoelcouselo@users.noreply.github.com>
2022-09-20 02:28:01 +0200

    Creacion de una navbar component con bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { RankingComponent } from './components/ranking/ranking.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    RankingComponent
   ],
   imports: [
     BrowserModule
